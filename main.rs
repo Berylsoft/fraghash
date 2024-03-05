98e4925005dd898bc8d2dc3e0e30f9fa14c6f0be
stackinspector <stackinspector@126.com>
2024-03-06 04:27:21 +0800

    move methods to foundations

--- main.rs
+++ main.rs
@@ -3,77 +3,8 @@ fn usize_u64(n: usize) -> u64 {
     n.try_into().expect("FATAL: usize length to u64 error")
 }
 
-use std::{fs::{self, OpenOptions}, io::{self, Read, Write}, path::{Path, PathBuf}};
-
-fn normalize(path: &Path) -> PathBuf {
-    use std::path::Component;
-    let mut components = path.components().peekable();
-    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek() {
-        let buf = PathBuf::from(c.as_os_str());
-        components.next();
-        buf
-    } else {
-        PathBuf::new()
-    };
-
-    for component in components {
-        match component {
-            Component::Prefix(..) => unreachable!(),
-            Component::RootDir => {
-                ret.push(component.as_os_str());
-            }
-            Component::CurDir => {}
-            Component::ParentDir => {
-                ret.pop();
-            }
-            Component::Normal(c) => {
-                ret.push(c);
-            }
-        }
-    }
-
-    ret
-}
-
-pub fn iter_path_core(
-    current_path: &Path,
-    depth: Option<u64>,
-    currect_depth: u64,
-    list: &mut Vec<(PathBuf, bool)>,
-) -> io::Result<()> {
-    for entry in fs::read_dir(current_path)? {
-        let entry = entry?;
-        let path = entry.path();
-        let is_dir = entry.file_type()?.is_dir();
-        if let Some(depth) = depth {
-            if currect_depth >= depth {
-                continue;
-            }
-        }
-        #[cfg(windows)]
-        if entry.file_name() == "System Volume Information" || entry.file_name() == "$RECYCLE.BIN" {
-            continue;
-        }
-        if is_dir {
-            iter_path_core(&path, depth, currect_depth + 1, list)?;
-        }
-        list.push((path, is_dir));
-    }
-    Ok(())
-}
-
-pub fn iter_path(path: &Path, depth: Option<u64>) -> io::Result<Vec<(PathBuf, bool)>> {
-    let mut list = Vec::new();
-    let is_dir = fs::metadata(path)?.file_type().is_dir();
-    if is_dir {
-        iter_path_core(path, depth, 0, &mut list)?;
-    } else {
-        list.push((path.to_owned(), is_dir));
-    }
-    list.sort_by(|a, b| a.0.cmp(&b.0));
-    Ok(list)
-}
-
+use std::{fs::OpenOptions, io::{self, Read, Write}, path::PathBuf};
+use foundations::fs::*;
 use cshake::{CShakeCustom, cshake_customs, Absorb, Squeeze, Reset};
 
 cshake_customs! {
@@ -107,7 +38,7 @@ fn main() {
         Box::new(io::stdout().lock())
     };
     let mut info_h = io::stderr().lock();
-    let mut buf = vec![0u8; frag];
+    let mut buf = vec![0; frag];
     let mut len_buf = itoa::Buffer::new();
     let mut hash_buf = vec![0; outsize];
     let mut hash_str_buf = vec![0; outsize * 2];
